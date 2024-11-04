8f14f3a1190b6092b5cf7cc9b46b1364577934c1
Ekaterina Petrova <ekaterinapetrova985@gmail.com>
2024-11-04 23:00:33 +0300

    Modules for architecture task

---
+++ Architecture/settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Architecture"
+include(":app")
+include(":common")
+include(":feature:list_users")
+include(":feature:chat")
+include(":feature:account")
+include(":feature:settings")
+include(":feature:authorization")
+include(":feature:registration")
